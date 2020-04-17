9313864e44296831616d584826cb9e1f6e9d0ef3
Shane Riley <shane@shaneriley.com>
2020-04-17 08:35:15 -0400

    Adds version query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { version } = require('../../package.json');
 
 module.exports = {
   info: () => `ACNH walls and floors collection manager v${version}`,
+  version: () => version,
   walls: async (parent, args, context) => {
     const walls = await context.prisma.walls();
     return walls;
